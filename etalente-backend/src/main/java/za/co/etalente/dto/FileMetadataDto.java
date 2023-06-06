811ebb8be06a46715ad507a5c3aac0ac70d496a0
Nhlamulo Socha <nchauke@csir.co.za>
2023-06-06 20:56:12 +0200

    added FileMetadata data transfer object. and Amazon S3 and Metadata Services are also added

---
+++ etalente-backend/src/main/java/za/co/etalente/dto/FileMetadataDto.java
@@ -0,0 +1,10 @@
+package za.co.etalente.dto;
+
+import lombok.Data;
+
+@Data
+public class FileMetadataDto {
+    private String fileName;
+    private String filePath;
+    private String version;
+}
