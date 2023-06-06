8cd7e37edc528c197fd92c008df581d514799576
Nhlamulo Socha <nchauke@csir.co.za>
2023-06-06 17:20:09 +0200

    init for backend code

---
+++ etalente-backend/src/main/java/za/co/etalente/S3AssessmentApplication.java
@@ -0,0 +1,13 @@
+package za.co.etalente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3AssessmentApplication.class, args);
+	}
+
+}
---
+++ etalente-backend/src/test/java/za/co/etalente/S3AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.etalente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
